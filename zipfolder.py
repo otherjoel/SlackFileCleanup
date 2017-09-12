ad1d592ffccd861d868050361d1ebcff1aea3190
Joel Dueck <joel@jdueck.net>
2017-09-12 09:48:59 -0500

    Clarify megabytes/bytes in size limit variables

--- zipfolder.py
+++ zipfolder.py
@@ -34,7 +34,7 @@ def max_mean_filesizes(folder):
     
     return max(file_sizes), statistics.median(file_sizes)
 
-def zip_folder(folder_path, zipfile_prefix, rough_size_limit=None):
+def zip_folder(folder_path, zipfile_prefix, rough_size_limit_mb=None):
     """Creates zip archive(s) of all files in a folder (non-recursive!)
     Can split the archive into multiple standalone .zip files in order
     to keep them manageable (e.g. for downloading).
@@ -47,12 +47,13 @@ def zip_folder(folder_path, zipfile_prefix, rough_size_limit=None):
     
     zipfile_prefix : str
         The prefix to use when creating .zip files (such as a YYYY-MM date). 
-        If multiple files are created, the suffix of '_001', '_002' will be
-        added before the .zip extension.
+        This can include a relative or absolute path. If multiple files are
+        created, the suffix of '_001', '_002' will be added before the .zip
+        extension.
         
-    rough_size_limit : int, optional
+    rough_size_limit_mb : int, optional
         How big each zip file should be allowed to grow before closing it and
-        starting a new one. If supplied, it MUST >= the size of the biggest
+        starting a new one. If supplied, it MUST be >= the size of the biggest
         file plus the median file size, to prevent weirdness. It's "rough"
         because the first file to bump the size over the limit will be left
         in the zip file and a new one started for the next file; so the limit
@@ -69,24 +70,24 @@ def zip_folder(folder_path, zipfile_prefix, rough_size_limit=None):
     if not os.path.isdir(folder_path):
         raise FileNotFoundError("%s is not a folder!" % folder_path)
     
-    if rough_size_limit is not None:
+    if rough_size_limit_mb is not None:
         biggest_filesize, median_filesize = max_mean_filesizes(folder_path)
-        min_limit = round((biggest_filesize + median_filesize) / 1000000,0)
+        min_mb_limit = round((biggest_filesize + median_filesize) / 1000000,0)
         
-        if rough_size_limit < min_limit:
+        if rough_size_limit_mb < min_mb_limit:
             raise ValueError("rough_size_limit must be at least %s MB for this folder" \
                              % min_limit)
         
         archive_split_count = 1
         zipfile_basename = zipfile_prefix + '_001'
-        rough_size_limit = rough_size_limit * 1000000
+        rough_size_limit_bytes = rough_size_limit_mb * 1000000
     else:
         zipfile_basename = zipfile_prefix
         
     archive_list = [ zipfile_basename + '.zip' ]
 
     zip_archive = zipfile.ZipFile(zipfile_basename + '.zip', mode='w') 
-    compressed_total = 0
+    compressed_total_bytes = 0
     
     for file, is_last_file in tell_last(os.listdir(folder_path)):
         full_file_path = os.path.join(folder_path, file)
@@ -98,20 +99,20 @@ def zip_folder(folder_path, zipfile_prefix, rough_size_limit=None):
             file_compressed = zipped_info.compress_size
             file_size = zipped_info.file_size
             
-            compressed_total += file_compressed
+            compressed_total_bytes += file_compressed
             
             if DEBUG:
                 print("Zipped %s, actual/compressed size: %s/%s bytes, total %s" % \
-                      (full_file_path, file_size, file_compressed, compressed_total))
+                      (full_file_path, file_size, file_compressed, compressed_total_bytes))
 
-            if rough_size_limit is not None:
-                if compressed_total >= rough_size_limit and not is_last_file:
+            if rough_size_limit_mb is not None:
+                if compressed_total_bytes >= rough_size_limit_bytes and not is_last_file:
                     archive_split_count += 1
                     zipfile_basename = zipfile_prefix + "_{0:0>3}".format(archive_split_count)
                 
                     zip_archive.close()
                     zip_archive = zipfile.ZipFile(zipfile_basename + '.zip', mode='w')
-                    compressed_total = 0
+                    compressed_total_bytes = 0
                     archive_list.append(zipfile_basename + '.zip')
 
                     if DEBUG:
